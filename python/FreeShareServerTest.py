0e803091b4fe0e03c2a4b035561e791cea3a8c3d
ShenlongXu <shenlongxu@afmobigroup.com>
2017-11-15 15:02:09 +0800

    debug freeshare protocol

--- python/FreeShareServerTest.py
+++ python/FreeShareServerTest.py
@@ -1,25 +1,30 @@
 # -*- coding:utf-8 -*-
+import hashlib
+import json
+import math
+import os
+import requests
+import sys
+import urllib
+
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
-import urllib2, sys, os
-import requests
-import hashlib
-import math
-#test 
-#python upload.py httpbin.org 80
-#Palmplay FreeShare
-#python upload.py 192.168.43.1 8087
- 
+
+# test
+# python upload.py httpbin.org 80
+# Palmplay FreeShare
+# python upload.py 192.168.43.1 8087
+
 # 在 urllib2 上注册 http 流处理句柄
 register_openers()
- 
+
 # 开始对文件 "DSC0001.jpg" 的 multiart/form-data 编码
 # "image1" 是参数的名字，一般通过 HTML 中的 <input> 标签的 name 参数设置
- 
+
 # headers 包含必须的 Content-Type 和 Content-Length
 # datagen 是一个生成器对象，返回编码过后的参数
 datagen, headers = multipart_encode({"file_upload": open(sys.argv[0], "rb")})
- 
+
 # 创建请求对象
 print sys.argv
 
@@ -28,7 +33,8 @@ port = sys.argv[2] if len(sys.argv) > 2 else '8087'
 baseUrl = "http://%s:%s/" % (server, port)
 clientName = "TestPython"
 clientId = hashlib.md5("Nobody inspects the spammish repetition").hexdigest()
-timeout = 10
+timeout = 120
+chunk_size = 8192
 '''
 request = urllib2.Request(requestUrl, datagen, headers)
 # 实际执行请求并取得返回
@@ -47,7 +53,7 @@ def stop_line():
 
 
 start_line("Test register")
-r = requests.get(baseUrl+"register/", params={"name": clientName, "id": clientId}, timeout=timeout)
+r = requests.get(baseUrl + "register/", params={"name": clientName, "id": clientId}, timeout=timeout)
 sessionToken = r.json()["token"]
 print r.json()
 stop_line()
@@ -55,13 +61,13 @@ stop_line()
 params = {"token": sessionToken, "id": clientId}
 
 start_line("Test login")
-r = requests.get(baseUrl+"token_request/", params=params, timeout=timeout)
+r = requests.get(baseUrl + "token_request/", params=params, timeout=timeout)
 print '"', r.content, '"', r.json()
 stop_line()
 # exit(1)
 
 start_line("Test request file list")
-r = requests.get(baseUrl+"request/", params=params, timeout=timeout)
+r = requests.get(baseUrl + "request/", params=params, timeout=timeout)
 print r.json()
 stop_line()
 
@@ -70,28 +76,52 @@ for item in r.json():
     reqParams = dict(params)
     reqParams["file_id"] = item["index"]
     print reqParams
-    r = requests.get(baseUrl+"get_file/", params=reqParams, timeout=timeout)
-    # print r.json()
+    r = requests.get(baseUrl + "get_file/", params=reqParams, timeout=timeout)
+    print r.headers
+    with open(item["name"], 'wb') as fd:
+        for chunk in r.iter_content(chunk_size):
+            fd.write(chunk)
+            # print r.json()
 stop_line()
 
 start_line("Test request get group")
-r = requests.get(baseUrl+"get_group/", params=params, timeout=timeout)
+r = requests.get(baseUrl + "get_group/", params=params, timeout=timeout)
 print r.json()
 stop_line()
 
 start_line("Test upload file API")
-file_data = {'file': (os.path.basename(sys.argv[0]), open(sys.argv[0], 'rb'), 'text/plain')}
-r = requests.request('post',
-                     baseUrl+"put_file/",
-                     data = { 'extra_data': "blah blah blah" , "filename": "ohmygood"},
-                     json=None,
-                     files = file_data)
+
+file_path = sys.argv[0]
+imei = u'708784188888888'
+mac = u'cb1d622f-03c0-4a59-9cec-7cff3dfd8724'
+metadata = {u'size': os.path.getsize(file_path),
+            u'index': 0,
+            u'name': os.path.basename(file_path),
+            u'packageName': u'',
+            u'isActived': False,
+            u'iconUrl': u'5.7.0.8',
+            u'mac': mac,
+            u'version': 5708,
+            u'isGrp': False,
+            u'coin': 0,
+            u'type': 6,
+            u'senderIMEI': imei}
+file_data = {'upload_file': (os.path.basename(file_path), open(file_path, 'rb'), 'text/plain'),
+             # 'file_metadata': (os.path.basename(file_path), open(file_path, 'rb'), 'application/json')
+             }
+upload_params = dict(params)
+# upload_params["file_metadata"] = json.dumps(metadata)
+
+r = requests.post(baseUrl + "put_file/" + "?" + urllib.urlencode(params),
+                  data={'file_metadata': json.dumps(metadata)},
+                  files=file_data,
+                  timeout=timeout)
 print r.status_code
 print r.headers['content-type']
 print r.headers
 print r.content
 stop_line()
 
-start_line("Test exit API")
-r = requests.get(baseUrl+"share_exit/", params=params, timeout=timeout)
-print r.content
+# start_line("Test exit API")
+# r = requests.get(baseUrl + "share_exit/", params=params, timeout=timeout)
+# print r.content
